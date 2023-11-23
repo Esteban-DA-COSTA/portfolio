62041d8872e1cb706d46783713f2d6c82a196faa
estebanDC <esteban.da_costa@outlook.fr>
2023-11-23 15:02:28 +0100

    PORTFOLIO-T-2 Develop Home Page

--- src/jsMain/kotlin/edu/estebandc/project/layout/HomePage.kt
+++ src/jsMain/kotlin/edu/estebandc/project/layout/HomePage.kt
@@ -1,12 +1,25 @@
 package edu.estebandc.project.layout
 
+import io.kvision.core.Background
+import io.kvision.core.BgRepeat
 import io.kvision.core.Container
+import io.kvision.core.UNIT
 import io.kvision.html.Div
 import io.kvision.html.div
 import io.kvision.html.h1
+import io.kvision.html.image
 
 fun Container.homePage(): Div {
+
     return div {
-        h1("Home page")
+        div {
+            background = Background(image = "images/custom-software-development.jpg", repeat = BgRepeat.NOREPEAT)
+            width = 100 to UNIT.vw
+            height = 100 to UNIT.vh
+
+        }
+        div {
+            h1("Home page")
+        }
     }
 }
