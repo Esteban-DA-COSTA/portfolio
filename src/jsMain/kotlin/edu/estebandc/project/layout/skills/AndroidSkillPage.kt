a83318d597c37bd0af39dd1e99609e07f8cd82ef
estebanDC <esteban.da_costa@outlook.fr>
2024-03-04 18:19:32 +0100

    Correct typos and improve skill section layout
    Fixed spelling errors found in AndroidSkillPage.kt and streamlined the "Mon niveau" section layout. This included consolidating information from multiple cards into one for clarity. The modifications improve readability and eloquently highlight the Android specific knowledge and future learning goals.

--- src/jsMain/kotlin/edu/estebandc/project/layout/skills/AndroidSkillPage.kt
+++ src/jsMain/kotlin/edu/estebandc/project/layout/skills/AndroidSkillPage.kt
@@ -39,7 +39,7 @@ fun Container.androidSkillPage(): Container {
                 p("Mes compétences dans ce domaine m'a permit de mener à bien l'implémentation de l'AppWidget, ce qui a permis de livrer cette fonctionnalité à la fin du projet avec le reste des attendus. Cette appWidget permet d'afficher les projets cours des utilisateurs sans avoir à ouvrir l'application.")
             }
             card("Les notifications") {
-                p("Un concept clé d'Andoid sont les notifications push. Ce sont des messages qu'une application peut envoyer au système que ce dernier affiche à l'utilisateur directement sur la page d'accueil ou la page de verrouillage.")
+                p("Un concept clé d'Android sont les notifications push. Ce sont des messages qu'une application peut envoyer au système que ce dernier affiche à l'utilisateur directement sur la page d'accueil ou la page de verrouillage.")
                 p("Lors de mon projet <a href='/${Category.PROJECTS.url}/${Project.LINKED.url}'>Linked</a>, j'ai développer ce système pour permettre aux utilisateurs de recevoir des notifications sur leurs commerces préférés.", rich = true)
                 p("Tout comme pour l'AppWidget, il a fallu pour mettre en place ces dernières, comprendre le fonctionnement d'Android pour les implémenter. Grâce à ces notifications, les utilisateurs n'ont pas besoin de lancer l'application pour ce tenir à jour des nouveautés et informations de leur quartier")
             }
@@ -47,16 +47,15 @@ fun Container.androidSkillPage(): Container {
         section("Mon niveau") {
             card("Ma maitrise") {
                 p("Bien qu'aillant effectuer plusieurs projets Android dans ma carrière, je ne considère mon niveau dans le domaine que d'<b>Autonome</b>. En effet, Android est un système complexe, et même si j'ai pu mener deux projets à bien, je ne suis pas aller en profondeur dans son fonctionnement.", rich = true)
-                p("De plus, les deux projets ont été effectués avec le langage Java plutôt que Kotlin. Il faut savoir que Google recommande l'utilisation de Kotlin pour le développement Android. L'utilisation de Java dans mon cas montre une connaissance succinte du système.")
+                p("De plus, les deux projets ont été effectués avec le langage Java plutôt que Kotlin. Il faut savoir que Google recommande l'utilisation de Kotlin pour le développement Android. L'utilisation de Java dans mon cas montre une connaissance succinct du système.")
+                p("Dans l'avenir, j'espère améliorer mes connaissance en Android grâce à des projets Kotlin. J'ai en effet appris ce langage de façon autodidacte, et son application en Android serait un bon moyen de m'améliorer dans les deux domaines.<br/>" +
+                        "Il existe un framework Kotlin appellé Jetpack, reconnu et soutenu par Google, permettant de développer des applications Android plus fiables et plus maintenables. J'aimerais effectuer des formations sur ce framework afin de m'améliorer.", rich = true)
             }
-            card("Ma marge de progression") {
-                p("Dans l'avenir, j'espère améliorer mes connaissance en Android grâce à des projets Kotlin. J'ai en effet appris ce langage de façon autodidacte, et son application en Android serait un bon moyen de m'améliorer dans les deux domaines.")
-                p("Il existe un framework Kotlin appellé Jetpack, reconnu et soutenu par Google, permettant de développer des applications Android plus fiables et plus maintenables. Il s'agit ici de la marge de progression à moyen terme de ma maitrise d'Android")
-            }
-            card("Mon appréciation") {
+            card("Sa place dans mon profil") {
                 p("Android est un domaine qui m'intéresse, dans lequel j'aimerais améliorer mes compétences dans un milieu professionnel. Plus précisément, j'aimerais développer mes compétences dans le langage Kotlin, le langage recommander pour le développement Android.")
                 p("Pour développer mes compétences dans ces deux domaines, je pense réaliser une application en utilisant le framework Jetpack Compose.")
             }
+            card("")
         }
     }
 }
